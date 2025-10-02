3c180adc111f0b6d7c0e1a93c1ec659c9019e071
john-asatryan <john.asatryan@picsart.academy>
2025-10-02 10:33:11 +0400

    feat: Lecture 02 - JS Types and Values

---
+++ classworks/1_foundations_js/data_types.js
@@ -0,0 +1,66 @@
+// // Data Types
+// // 1. typeof
+
+// // var x = 12;
+// // var y = 3.15;
+
+// // var b = 0b101;
+// // var b = 0xf;
+
+// // operator vs function
+// // + vs __add
+
+// // var t = 1_000_000;
+
+// // var s = .45;
+// // console.log(s)
+
+// // Number object
+
+// let x = 3.14;
+
+// console.log(x.toFixed(10));
+
+// // const tmp = new Number(x);
+// // tmp.toFixed(10);
+// // delete tmp;
+
+// console.log(typeof x);
+
+// const numberObject = new Number('12');
+// console.log(typeof numberObject);
+
+// struct Person { };
+
+// void toSleep(Person* self);
+
+// let a = BigInt(12); // 12n
+// var s1 = Symbol('hello');
+// var s2 = Symbol('hello');
+
+// var str1 = 'hello';
+// var str2 = 'hello';
+
+// let obj = {
+//   [s1]: 'first value',
+//   [s2]: 'second value',
+// };
+
+// string // utf-8
+
+console.log('hello'.toUpperCase());
+
+// char ch = 'a';
+// int x = int(ch); -> implicit cast
+
+let x = 12;
+
+// console.log(typeof String(x));
+
+// console.log(0 / 0);
+
+// console.log(5 / 2);
+// var size = 13;
+// for (var i = 0; i < size / 2; ++i) {
+//   console.log('hello world');
+// }
